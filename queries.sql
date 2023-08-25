1404435931a9b37020c387f53730d3e964daf02c
jaydwivedi12 <jayprakashdwivedi12@gmail.com>
2023-08-25 15:14:52 +0530

    where, in, between,logical

--- queries.sql
+++ queries.sql
@@ -1,57 +1,84 @@
-show databases;
-use sakila;
-show tables;
-describe actor;
-desc actor;
+SHOW DATABASES;
+USE sakila;
+SHOW TABLES;
+DESCRIBE actor;
+DESC actor;
 
 -- DQL
-select * from actor;
-select actor_id,first_name from aCtOr;
-desc actor_info;
+SELECT * FROM actor;
+SELECT actor_id,first_name FROM aCtOr;
+DESC actor_info;
 
 -- projection and selection
 -- selecting a column is projection
 -- selecting a row is selection
 
-select  actor_id, actor_id+5 from actor_info;
-select actor_id from actor_info; -- this is projection
-select * from actor_info where actor_id=2; -- this is selection
-select actor_id,first_name from actor_info where actor_id>2; -- this is both projection and selection both
+SELECT  actor_id, actor_id+5 FROM actor_info;
+SELECT actor_id FROM actor_info; -- this is projection
+SELECT * FROM actor_info WHERE actor_id=2; -- this is selection
+SELECT actor_id,first_name FROM actor_info WHERE actor_id>2; -- this is both projection and selection both
 
-select first_name from actor_info where actor_id<10;
-select * from actor_info where first_name="ED";
+SELECT first_name FROM actor_info WHERE actor_id<10;
+SELECT * FROM actor_info WHERE first_name="ED";
 
 -- 24 august
-select database();  --  gives current working database 
-show columns from actor_info;
-desc actor_info;
+SELECT DATABASE();  --  gives current working database 
+SHOW COLUMNS FROM actor_info;
+DESC actor_info;
 
-select * from sakila.actor; -- access db table without going into that DB
+SELECT * FROM sakila.actor; -- access db table without going into that DB
 
-select name from language;
+SELECT name FROM language;
 
 -- where 
-select * from language where name='English';
-select * from language where language_id != 3;
+SELECT * FROM language WHERE name='English';
+SELECT * FROM language WHERE language_id != 3;
 
-select * from actor where first_name > 'm';
+SELECT * FROM actor WHERE first_name > 'm';
 
 -- wild card character 
-select * from film;
-select * from film where title like '%dinosaur%';
-select * from film where description like "%drama%";
-select title,description,release_year from film where description like "a%s";
-select * from film where title like 'A_A%';
+SELECT * FROM film;
+SELECT * FROM film WHERE title LIKE '%dinosaur%';
+SELECT * FROM film WHERE description LIKE "%drama%";
+SELECT title,description,release_year FROM film WHERE description LIKE "a%s";
+SELECT * FROM film WHERE title LIKE 'A_A%';
 
 -- Question
-select title,film_id,release_year,description from film where title like '_c%';
-select title,film_id,release_year,description from film where title like 'A%AN';
-select title,film_id,release_year,description from film where title like '%T___';
-select title,film_id,release_year,description from film where title like '_f_i%egg';
-select title,film_id,release_year,description from film where title like '__A__%';
+SELECT title,film_id,release_year,description FROM film WHERE title LIKE '_c%';
+SELECT title,film_id,release_year,description FROM film WHERE title LIKE 'A%AN';
+SELECT title,film_id,release_year,description FROM film WHERE title LIKE '%T___';
+SELECT title,film_id,release_year,description FROM film WHERE title LIKE '_f_i%egg';
+SELECT title,film_id,release_year,description FROM film WHERE title LIKE '__A__%';
 
+-- logical operators
 
+SELECT * FROM actor WHERE first_name="nick" AND actor_id=2;
+SELECT * FROM actor WHERE first_name='nick' OR actor_id=3;
 
+SHOW TABLES;
+SELECT * FROM actor_info ;
 
+SELECT * FROM actor_info WHERE film_info LIKE "ANIMATION%" AND first_name="GOLDIE";
+SELECT * FROM actor_info WHERE film_info LIKE "ANIMATION%" AND first_name="GOLDIE" OR first_name="nick";
+SELECT * FROM actor_info WHERE film_info LIKE "ANIMATION%" OR first_name="GOLDIE" AND first_name="nick";
+
+SELECT * FROM film_list;
+SELECT category,title,rating FROM film_list WHERE category LIKE 'SCI-FI' AND rating LIKE "PG";
+SELECT category,rating,title FROM film_list WHERE (category LIKE 'sci-fi' OR category LIKE 'family') AND rating='pg';
+
+SELECT 2 + 2 * 3;
+SELECT (2 + 2) * 3;
+
+SELECT * FROM actor
+WHERE actor_id BETWEEN 2 AND 5; -- between is inclusive 
+
+SELECT * FROM actor 
+WHERE first_name BETWEEN 'b' AND 'e';
+
+SELECT * FROM actor 
+WHERE actor_id IN (1,5,10,15);
+
+select * from actor
+where not(actor_id=1);
 
 
